406dce98c09cc66108c687aefb009dbf696d29cd
hyun0404woo <hyun0404woo@gmail.com>
2022-10-13 21:05:29 +0900

    feat:  OpenApi(swagger) 적용

--- src/main/java/org/goormton/darktourism/DarktourismApplication.java
+++ src/main/java/org/goormton/darktourism/DarktourismApplication.java
@@ -1,8 +1,12 @@
 package org.goormton.darktourism;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@OpenAPIDefinition
 @SpringBootApplication
 public class DarktourismApplication {
 
