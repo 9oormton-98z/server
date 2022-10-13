ef140b2b529ff218b30232597321f69dd5c969e3
hyun0404woo <hyun0404woo@gmail.com>
2022-10-13 21:05:54 +0900

    feat: member service 생성

---
+++ src/main/java/org/goormton/darktourism/service/member/MemberService.java
@@ -0,0 +1,21 @@
+package org.goormton.darktourism.service.member;
+
+
+import org.goormton.darktourism.controller.auth.dto.LoginDto;
+import org.goormton.darktourism.domain.Member;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public interface MemberService {
+
+    /**
+     * 유저가 이미 있는지 확인
+     * 있다면 해당 유저 리턴
+     * 없다면 새로 생성
+     */
+     Member findMemberByNickname(LoginDto loginDto);
+
+    Member findMemberBycookie(Cookie[] cookies);
+    
+}
---
+++ src/main/java/org/goormton/darktourism/service/member/MemberServiceImpl.java
@@ -0,0 +1,41 @@
+package org.goormton.darktourism.service.member;
+
+import lombok.RequiredArgsConstructor;
+import org.goormton.darktourism.controller.auth.dto.LoginDto;
+import org.goormton.darktourism.domain.Member;
+import org.goormton.darktourism.exception.member.MemberNotFoundException;
+import org.goormton.darktourism.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.Cookie;
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional
+    public Member findMemberByNickname(LoginDto loginDto) {
+        final String nickname = loginDto.getNickname();
+        return memberRepository.findMemberByNickname(nickname)
+                .orElseGet(() -> 
+                    memberRepository.save(Member.createMember(nickname)));
+    }
+
+    @Override
+    public Member findMemberBycookie(Cookie[] cookies) {
+        final String nickname = Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("accessToken"))
+                .collect(Collectors.toList())
+                .get(0)
+                .getValue();
+
+        return memberRepository.findMemberByNickname(nickname)
+                .orElseThrow(MemberNotFoundException::new);
+    }
+}
