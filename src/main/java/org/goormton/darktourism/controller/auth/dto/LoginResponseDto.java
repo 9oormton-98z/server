0a5e714bdabc7c0bdf9f49b42a782bd2ca086e93
hyun0404woo <hyun0404woo@gmail.com>
2022-12-06 10:43:13 +0900

    feat: objectMapper를 위한 어노테이션 설정

--- src/main/java/org/goormton/darktourism/controller/auth/dto/LoginResponseDto.java
+++ src/main/java/org/goormton/darktourism/controller/auth/dto/LoginResponseDto.java
@@ -2,9 +2,11 @@ package org.goormton.darktourism.controller.auth.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponseDto {
     private String accessToken;
 }
