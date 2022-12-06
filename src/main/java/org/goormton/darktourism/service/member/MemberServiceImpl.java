f5a09425890e69d1b03d779882c8983ea9f10e6e
hyun0404woo <hyun0404woo@gmail.com>
2022-12-06 10:36:06 +0900

    feat: 로그인시 토큰 생성 기능 구현

--- src/main/java/org/goormton/darktourism/service/member/MemberServiceImpl.java
+++ src/main/java/org/goormton/darktourism/service/member/MemberServiceImpl.java
@@ -2,9 +2,14 @@ package org.goormton.darktourism.service.member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.goormton.darktourism.controller.auth.dto.LoginRequestDto;
 import org.goormton.darktourism.domain.member.Member;
+import org.goormton.darktourism.domain.member.MemberRole;
 import org.goormton.darktourism.exception.member.MemberNotFoundException;
 import org.goormton.darktourism.repository.member.MemberRepository;
+import org.goormton.darktourism.repository.member.MemberRoleRepository;
+import org.goormton.darktourism.security.jwt.JwtUtil;
+import org.goormton.darktourism.service.auth.dto.LoginToken;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.Cookie;
@@ -18,30 +23,42 @@ import java.util.stream.Collectors;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final MemberRoleRepository memberRoleRepository;
+    private final JwtUtil jwtUtil;
 
     @Override
     @Transactional
     public Member findMemberByNickname(String nickname) {
         if (nickname == null) {
             log.info("MemberServiceImpl nickname : " + nickname);
-            
             throw new MemberNotFoundException();
-            
         }
-        return memberRepository.findMemberByNickname(nickname)
+
+        final MemberRole role = memberRoleRepository.findMemberRoleByRoleName("ROLE_MEMBER");
+        
+        return memberRepository.findMemberByUsername(nickname)
                 .orElseGet(() -> 
-                    memberRepository.save(Member.createMember(nickname)));
+                    memberRepository.save(Member.createMember(nickname, role)));
     }
 
     @Override
-    public Member findMemberBycookie(Cookie[] cookies) {
+    public LoginToken createLoginToken(LoginRequestDto loginRequestDto) {
+        final Member member = memberRepository.findMemberByUsername(loginRequestDto.getUsername())
+                .orElseThrow(MemberNotFoundException::new);
+        final String accessToken = jwtUtil.generateAccessToken(member);
+        final String refreshToken = jwtUtil.generateRefreshToken(member);
+        return new LoginToken(accessToken, refreshToken);
+    }
+
+    @Override
+    public Member findMemberByCookie(Cookie[] cookies) {
         final String nickname = Arrays.stream(cookies)
                 .filter(cookie -> cookie.getName().equals("accessToken"))
                 .collect(Collectors.toList())
                 .get(0)
                 .getValue();
 
-        return memberRepository.findMemberByNickname(nickname)
+        return memberRepository.findMemberByUsername(nickname)
                 .orElseThrow(MemberNotFoundException::new);
     }
 }
