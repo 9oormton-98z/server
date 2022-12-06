8e3bc7e3a7c76b830e592f3f10e86bd9375c3264
hyun0404woo <hyun0404woo@gmail.com>
2022-12-06 10:39:05 +0900

    feat: MemberRole 엔티티 생성
    - 일반 유저, 어드민 유저를 구분해 권한을 부여하기 위한 엔티티 생성

--- src/main/java/org/goormton/darktourism/domain/member/Member.java
+++ src/main/java/org/goormton/darktourism/domain/member/Member.java
@@ -1,10 +1,12 @@
 package org.goormton.darktourism.domain.member;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.goormton.darktourism.domain.badge.BadgeMember;
 import org.goormton.darktourism.domain.place.PlaceStarMember;
+import org.goormton.darktourism.security.auth.PrincipalDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -22,9 +24,18 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    private String nickname;
+    private String username;
 
+    private String password;
+    
     private Double point = 0.0;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name="user_roles",
+            joinColumns = {@JoinColumn(name = "member_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")})
+    private List<MemberRole> roles = new ArrayList<>();
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "member")
     private final List<BadgeMember> badgeMembers = new ArrayList<>();
@@ -32,12 +43,18 @@ public class Member {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "member")
     private final List<PlaceStarMember> placeStarMembers = new ArrayList<>();
 
-    private Member(String nickname) {
-        this.nickname = nickname;
+    private Member(String username, MemberRole role) {
+        this.username = username;
+        this.password = "1234";
+        roles.add(role);
     }
 
-    public static Member createMember(String nickname) {
-        return new Member(nickname);
+    public static Member createMember(String nickname, MemberRole role) {
+        return new Member(nickname, role);
+    }
+
+    public PrincipalDetails toPrincipalDetails() {
+        return new PrincipalDetails(this);
     }
 
     public void addPoint(Double addPoint) {
