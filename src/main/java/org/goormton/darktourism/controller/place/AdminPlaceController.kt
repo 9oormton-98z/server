72e38eb8d6243ac7189ae332a4fe1276061c9af2
hyun0404woo <hyun0404woo@gmail.com>
2022-12-08 23:05:43 +0900

    feat: 유적지 개별 추가 기능 API 인터페이스 생성

--- src/main/java/org/goormton/darktourism/controller/place/AdminPlaceController.kt
+++ src/main/java/org/goormton/darktourism/controller/place/AdminPlaceController.kt
@@ -1,9 +1,11 @@
 package org.goormton.darktourism.controller.place
 
+import org.goormton.darktourism.controller.place.dto.CreatePlaceRequestDto
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 
 @RequestMapping("/api/v1/admin/place")
 interface AdminPlaceController {
@@ -16,7 +18,11 @@ interface AdminPlaceController {
     /**
      * 관리자 페이지 -> 유적지 추가 기능
      */
-    @PostMapping(value = [""])
-    fun addPlace(): ResponseEntity<*>?
+    @PostMapping(value = [""], consumes = [APPLICATION_JSON_VALUE, MULTIPART_FORM_DATA_VALUE])
+    fun addPlace(
+//        @RequestBody createPlaceRequestDto: CreatePlaceRequestDto,
+        @RequestPart("data") createPlaceRequestString: String,
+        @RequestPart("file") files: List<MultipartFile>
+    ): ResponseEntity<*>?
     
 }
